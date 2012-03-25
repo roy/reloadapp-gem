ab2a715980f0254fed9ba0d364235b8f9f6a5400
Roy van der Meij <roy@royapps.nl>
2012-03-25 22:11:13 +0200

    testing events

--- lib/reloadapp.rb
+++ lib/reloadapp.rb
@@ -1,5 +1,19 @@
 require "reloadapp/version"
+require "rb-fsevent"
 
 module Reloadapp
-  # Your code goes here...
+  def self.run
+    on_change { change_event }
+  end
+
+  def self.change_event
+    puts "You've changes something!" 
+  end
+
+  def self.on_change &block
+    FSEvent.new.tap do |fsevent|
+      fsevent.watch(., &block)
+      fsevent.run
+    end
+  end
 end
